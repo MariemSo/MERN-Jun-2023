dd79e98295e7ee4d73fe48a38f4fefe893abefe9
MariemSo <souayah.mariem@gmail.com>
2023-06-21 06:53:14 +0100

    Health wrong

--- Week1/Day 2/Super_Ninja.js
+++ Week1/Day 2/Super_Ninja.js
@@ -21,3 +21,22 @@ class Ninja{
 const ninja1 = new Ninja ("Hybusa");
 ninja1.sayName();
 ninja1.showStats();
+
+class Sensei extends Ninja{
+    constructor (name,wisdom=10){
+        super(name,"200","10","10")
+        this.wisdom = wisdom
+    }
+
+    speakWisdom(){
+        this.drinkSake()
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
